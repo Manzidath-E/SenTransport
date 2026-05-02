5c1b88a1f65109378663424fff6b4ebc8dace1de
Manzidath Abèdjè ENIAYEOU <eniayeoumanzidath@gmail.com>
2026-05-02 09:42:09 +0000

    Lab 2 : exercices couleur, StatReseau et lignes suplémentaires

--- src/App.js
+++ src/App.js
@@ -2,25 +2,33 @@ import './App.css';
 import Header from './Header';
 import ListeLignes from './ListeLignes';
 import Footer from './Footer';
+import StatReseau from './StatReseau';
 
 function App() {
   const lignes = [
-    { id: 1, numero: "1",  depart: "Parcelles Assainies", arrivee: "Plateau",     arrets: 14 },
-    { id: 2, numero: "7",  depart: "Guediawaye",          arrivee: "Place Obe",   arrets: 18 },
-    { id: 3, numero: "15", depart: "Pikine",              arrivee: "Medina",      arrets: 12 },
-    { id: 4, numero: "23", depart: "Ouakam",              arrivee: "Grand Dakar", arrets: 10 },
-    { id: 5, numero: "8",  depart: "Almadies",            arrivee: "Colobane",    arrets: 16 },
-    { id: 6, numero: "12", depart: "Yoff",                arrivee: "Sandaga",     arrets: 11 },
-  ];
+    { id: 1, numero: "1",   depart: "Parcelles Assainies", arrivee: "Plateau",     arrets: 14, couleur: "#e74c3c" },
+    { id: 2, numero: "7",   depart: "Guediawaye",          arrivee: "Place Obe",   arrets: 18, couleur: "#3498db" },
+    { id: 3, numero: "15",  depart: "Pikine",              arrivee: "Medina",      arrets: 12, couleur: "#9b59b6" },
+    { id: 4, numero: "23",  depart: "Ouakam",              arrivee: "Grand Dakar", arrets: 10, couleur: "#e67e22" },
+    { id: 5, numero: "8",   depart: "Almadies",            arrivee: "Colobane",    arrets: 16, couleur: "#1abc9c" },
+    { id: 6,  numero: "12",  depart: "Yoff",                arrivee: "Sandaga",     arrets: 11, couleur: "#e91e63" },
+    { id: 7,  numero: "31", depart: "Fann",               arrivee: "HLM",         arrets: 9,  couleur: "#27ae60" },
+    { id: 8,  numero: "42", depart: "Liberté",            arrivee: "Medina",      arrets: 13, couleur: "#f39c12" },
+    { id: 9,  numero: "18", depart: "Dieuppeul",          arrivee: "Sandaga",     arrets: 15, couleur: "#2980b9" },
+    { id: 10, numero: "25", depart: "Sicap",              arrivee: "Colobane",    arrets: 11, couleur: "#c0392b" },
+ 
+];
 
   return (
     <div className="App">
       <Header />
       <main className="contenu">
+        <StatReseau lignes={lignes} />
         <ListeLignes lignes={lignes} />
       </main>
       <Footer />
     </div>
+    
   );
 }
 
