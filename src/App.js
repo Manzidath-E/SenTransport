7e49bae9c26bbf2bdc42aef51d650947a7309b6a
Manzidath-E <eniayeoumanzidath@gmail.com>
2026-06-14 17:47:11 +0000

    Lab 6 : carte Leaflet avec arrets et geolocalisation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Recherche from './Recherche';
 import LigneBus from './LigneBus';
 import DetailLigne from './DetailLigne';
 import Footer from './Footer';
+import Carte from './Carte';
 
 
 function App() {
@@ -132,6 +133,7 @@ function App() {
           />
         ))}
         {ligneSelectionnee && <DetailLigne ligne={ligneSelectionnee} />}
+        <Carte />
       </main>
       <Footer />
     </div>
